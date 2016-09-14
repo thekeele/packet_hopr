3c6c926e43d37401cd5abd2b7c661e39fc04bb22
Mark Keele <mark@onxmaps.com>
2016-09-14 11:25:15 -0600

    verbose comments

--- server/api.rb
+++ server/api.rb
@@ -1,38 +1,49 @@
 #!/usr/bin/env ruby
 
+# use RACK_ENV environment or default to development
 ENV['RACK_ENV'] ||= 'development'
 
+# sinatra / ruby libs, sinatra/base required for modular style
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'sinatra/config_file'
 require 'json'
 
+# relative paths to user written libs
 require_relative 'class/packet_hopr'
 require_relative 'routing/routes'
 
+# modular style app, inherits from Sinatra Base
 class PacketHoprAPI < Sinatra::Base
+  # register and load yaml config file
   register Sinatra::ConfigFile
   config_file 'config.yml'
 
+  # set the root dir of the app
   set :root, settings.root
 
+  # development, test specific settings
   configure :development, :test do
     enable :logging
     register Sinatra::Reloader
 
+    # loaded from config.yml
     set :bind, settings.development[:bind]
     set :port, settings.development[:port]
     set :run, settings.development[:run]
     set :server, settings.development[:server]
   end
 
+  # production specific settings
   configure :production do
+    # loaded from config.yml
     set :bind, settings.production[:bind]
     set :port, settings.production[:port]
     set :run, settings.production[:run]
     set :server, settings.production[:server]
   end
 
+  # register Routes module with Sinatra
   register Sinatra::PacketHoprAPI::Routing::Routes
 
   # start the server if ruby file executed directly
--- server/routing/routes.rb
+++ server/routing/routes.rb
@@ -1,36 +1,53 @@
 require 'sinatra/base'
 
+# nested modules based on register in api.rb
 module Sinatra
   module PacketHoprAPI
     module Routing
       module Routes
+        # define a function that's registered as the app from api.rb
         def self.registered(app)
+          # before any request is processed, run this block
           app.before do
+            # all requests serve json
             content_type :json
 
+            # this app does not allow nested slashes
             if request.path_info.count('/') > 1
+              # more than one slash results in error
               redirect '/error'
             end
           end
 
+          # default route, will be removed as some point
           app.get '/' do
             JSON.pretty_generate({key: 'Welcome to PacketHopr', value: 'add /DOMAIN_NAME to see your path'})
           end
 
+          # dynamic route, expected a valid domain name
           app.get '/:domain' do
+            # pull the domain name from parameters
             domain = params[:domain]
+            # what constitutes a valid domain name
             regex = /^[a-zA-Z0-9][a-zA-Z0-9\-_]{0,61}[a-zA-Z0-9]{0,1}\.([a-zA-Z]{1,6}|[a-zA-Z0-9\-]{1,30}\.[a-zA-Z]{2,3})$/
 
+            # match will return nil if a match is not found
             if regex.match(domain).nil?
+              # not an error, bad user input
               JSON.pretty_generate({key: 'Uh Oh', value: 'I suspect an invalid domain name'})
             else
+              # set up vars needed to process a domain name
               path = {}
               hops = []
+              # instantiate a PacketHopr object initialized with the supplied domain
               hopr = PacketHopr.new(domain)
 
+              # traceroute returns an array of ip addresses
               hops = hopr.traceroute
+              # hops are supplied to location, returns a hash of hop location data
               path = hopr.location(hops)
 
+              # render the path of the domain name
               JSON.pretty_generate(path)
             end
           end
